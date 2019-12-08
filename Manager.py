18c8c9450a276067bc652133e9bd901c173b3508
RuanZA <ruanerasmus29@gmail.com>
2019-12-08 18:30:21 +0200

    added file system

--- Manager.py
+++ Manager.py
@@ -13,19 +13,29 @@ import json
 import time
 
 
-
-
 class MyHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
             src = folder_to_track + '/' + filename
-            #if (mimetypes.guess_type(filename) == )
-            new_dest = folder_dest + '/' + filename
+            # if (mimetypes.guess_type(filename) == )
+            if filename.endswith('.exe'):
+                new_dest = folder_exe + '/' + filename
+            elif filename.endswith('.jpg') or filename.endswith('.gif') or filename.endswith('.png'):
+                new_dest = folder_img + '/' + filename
+            elif filename.endswith('.zip'):
+                new_dest = folder_zip + '/' + filename
+            else:
+                new_dest = folder_unkown + '/' + filename
+
             os.rename(src, new_dest)
 
-folder_to_track = "C:\\Users\\PeeWeeZA\\Desktop\\one"
-folder_dest = "C:\\Users\\PeeWeeZA\\Desktop\\two"
+
+folder_to_track = "E:\\Downloads"
+folder_exe = "E:\\downloads organized\\exe"
+folder_img = "E:\\downloads organized\\images"
+folder_zip = "E:\\downloads organized\\zips"
+folder_unkown = "E:\\downloads organized\\unkown"
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # FileManager
 # Need to get the file extensions of the files and sort them by type
 # add each sort to a new folder in order to have a sorted downloads folder
+
+10 image types
+JPEG (or JPG) - Joint Photographic Experts Group
+PNG - Portable Network Graphics
+GIF - Graphics Interchange Format
+TIFF - Tagged Image File
+PSD - Photoshop Document
+PDF - Portable Document Format
+EPS - Encapsulated Postscript
+AI - Adobe Illustrator Document
+INDD - Adobe Indesign Document
+RAW - Raw Image Formats
